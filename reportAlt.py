32577adc093c2696269837ce5b10a5235c8a4a3d
Jason Ji <jimanproductions@gmail.com>
2023-12-03 14:02:02 +0100

    updated fucntion positoin again

--- reportAlt.py
+++ reportAlt.py
@@ -11,29 +11,26 @@ temp = sense.get_temperature()
 pressure = sense.get_pressure()
 
 
-def calculate_altitude(pressure, sea_level_pressure=1013.25):
-    # Constants
-    molar_mass_air = 0.029  # kg/mol
-    gravitational_acceleration = 9.81  # m/s^2
-    universal_gas_constant = 8.314  # J/(mol*K)
-
-    kentucky_modifier = 0
-    # Calculate temperature in Kelvin (you'll need to provide this)
-    temperature_in_celsius = temp  # Replace with the actual temperature in Celsius
-    temperature_in_kelvin = temperature_in_celsius + 273.15
-
-    # Calculate altitude using the barometric formula
-    altitude = ((-universal_gas_constant * temperature_in_kelvin) / 
-                (molar_mass_air * gravitational_acceleration) * 
-                math.log(pressure / sea_level_pressure)) - kentucky_modifier
-
-    return altitude
-
-
-
-
 # Function to get sensor data and write to a JSON file
 def get_and_save_sensor_data():
+    def calculate_altitude(pressure, sea_level_pressure=1013.25):
+        # Constants
+        molar_mass_air = 0.029  # kg/mol
+        gravitational_acceleration = 9.81  # m/s^2
+        universal_gas_constant = 8.314  # J/(mol*K)
+
+        kentucky_modifier = 0
+        # Calculate temperature in Kelvin (you'll need to provide this)
+        temperature_in_celsius = temp  # Replace with the actual temperature in Celsius
+        temperature_in_kelvin = temperature_in_celsius + 273.15
+
+        # Calculate altitude using the barometric formula
+        altitude = ((-universal_gas_constant * temperature_in_kelvin) / 
+                    (molar_mass_air * gravitational_acceleration) * 
+                    math.log(pressure / sea_level_pressure)) - kentucky_modifier
+
+        return altitude
+    
     while True:
         # Get sensor data
         # Example usage
