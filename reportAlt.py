6ccad802382187a9c06c8ad85f46223bf82f0032
Jason Ji <jimanproductions@gmail.com>
2023-12-03 14:04:28 +0100

    corrected get sense

--- reportAlt.py
+++ reportAlt.py
@@ -7,33 +7,39 @@ from sense_hat import SenseHat
 
 # Initialize the SenseHat object
 sense = SenseHat()
-temp = sense.get_temperature()
-pressure = sense.get_pressure()
+sense.clear()
+
+
+
+def calculate_altitude(pressure, sea_level_pressure=1013.25):
+    # Constants
+    molar_mass_air = 0.029  # kg/mol
+    gravitational_acceleration = 9.81  # m/s^2
+    universal_gas_constant = 8.314  # J/(mol*K)
+
+    kentucky_modifier = 0
+    # Calculate temperature in Kelvin (you'll need to provide this)
+    temperature_in_celsius = temp  # Replace with the actual temperature in Celsius
+    temperature_in_kelvin = temperature_in_celsius + 273.15
+
+    # Calculate altitude using the barometric formula
+    altitude = ((-universal_gas_constant * temperature_in_kelvin) / 
+                (molar_mass_air * gravitational_acceleration) * 
+                math.log(pressure / sea_level_pressure)) - kentucky_modifier
+
+    return altitude
+
+
 
 
 # Function to get sensor data and write to a JSON file
 def get_and_save_sensor_data():
-    def calculate_altitude(pressure, sea_level_pressure=1013.25):
-        # Constants
-        molar_mass_air = 0.029  # kg/mol
-        gravitational_acceleration = 9.81  # m/s^2
-        universal_gas_constant = 8.314  # J/(mol*K)
-
-        kentucky_modifier = 0
-        # Calculate temperature in Kelvin (you'll need to provide this)
-        temperature_in_celsius = temp  # Replace with the actual temperature in Celsius
-        temperature_in_kelvin = temperature_in_celsius + 273.15
-
-        # Calculate altitude using the barometric formula
-        altitude = ((-universal_gas_constant * temperature_in_kelvin) / 
-                    (molar_mass_air * gravitational_acceleration) * 
-                    math.log(pressure / sea_level_pressure)) - kentucky_modifier
-
-        return altitude
-    
     while True:
         # Get sensor data
         # Example usage
+        temp = sense.get_temperature()
+        pressure = sense.get_pressure()
+        
         sea_level_pressure = 1013.25  # Standard sea level pressure in hPa or millibars
         measured_pressure = pressure  # Replace with the measured atmospheric pressure in hPa or millibars
         MeasuredAltitude = calculate_altitude(measured_pressure, sea_level_pressure)
